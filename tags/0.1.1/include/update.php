4ea9a11c627b57b011ea7fedff1a0fd1099d49c5
fantasyworld <fantasyworld@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-30 04:47:58 +0000

    git-svn-id: https://plugins.svn.wordpress.org/upload-to-ftp@538079 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.1.1/include/update.php
@@ -0,0 +1,77 @@
+<?php
+$version = get_option('U2FTP_version', '0.1.1');
+if( version_compare($version, '0.0.6', '<') ) {
+	update_to_0_0_6();
+}
+if( version_compare($version, '0.0.8', '<') ) {
+	update_to_0_0_8();
+}
+if( version_compare($version, '0.0.9', '<') ) {
+	update_to_0_0_9();
+}
+if( version_compare($version, '0.1.0', '<') ) {
+	update_to_0_1_0();
+}
+
+function update_to_0_0_6() {
+	global $wpdb;
+	$postmetas = $wpdb->get_results('SELECT post_id FROM ' . $wpdb->postmeta . ' WHERE meta_key LIKE "file_to_ftp"');
+	if( $postmetas ) {
+		foreach( $postmetas as $postmeta ) {
+			$meta_date = get_post_meta($postmeta->post_id, 'file_to_ftp', true);
+			if( !is_array($meta_date) ) {
+				$dir = wp_upload_dir(get_the_time('Y-m-d H:i:s', $postmeta->post_id));
+				$metadate = array('up_time' => $meta_date, 'up_dir' => $dir['subdir']);
+				if( substr($metadate['up_dir'], 0, 1) == '/' ) {
+					$metadate['up_dir'] = substr($metadate['up_dir'], 1);
+				}
+				if( substr($ftp_dir, -1) == '/' ) {
+					$metadate['up_dir'] = substr($metadate['up_dir'], 0, -1);
+				}
+				update_post_meta($postmeta->post_id, 'file_to_ftp', $metadate);
+			}
+		}
+	}
+	update_option('U2FTP_version', '0.0.6');
+}
+
+function update_to_0_0_8() {
+	$u2ftp_options = get_option('U2FTP_options', array());
+	$u2ftp_options['auto_delete_local'] = 0;
+	$u2ftp_options['save_original_file'] = 1;
+	update_option('U2FTP_options', $u2ftp_options);
+	update_option('U2FTP_version', '0.0.8');
+}
+
+function update_to_0_0_9() {
+	global $wpdb;
+	$postmetas = $wpdb->get_results('SELECT post_id FROM ' . $wpdb->postmeta . ' WHERE meta_key LIKE "file_to_ftp"');
+	if( $postmetas ) {
+		foreach( $postmetas as $postmeta ) {
+			$meta_date = get_post_meta($postmeta->post_id, 'file_to_ftp', true);
+			if( strlen($meta_date['up_dir']) > 1 ) {
+				if( substr($meta_date['up_dir'], -1) != '/' ) {
+					$meta_date['up_dir'] .= '/';
+					echo($meta_date['up_dir']);
+					update_post_meta($postmeta->post_id, 'file_to_ftp', $meta_date);
+				}
+			} else {
+				$meta_date['up_dir'] = '';
+				update_post_meta($postmeta->post_id, 'file_to_ftp', $meta_date);
+			}
+		}
+	}
+	update_option('U2FTP_version', '0.0.9');
+}
+
+function update_to_0_1_0() {
+	$u2ftp_options = get_option('U2FTP_options', array());
+	if( $u2ftp_options['ftp_uplode_ok'] && $u2ftp_options['ftp_delete_ok'] ) {
+		$u2ftp_options['html_file_line_ok'] = true;
+	} else {
+		$u2ftp_options['html_file_line_ok'] = false;
+	}
+	update_option('U2FTP_options', $u2ftp_options);
+	update_option('U2FTP_version', '0.1.0');
+}
+?>
